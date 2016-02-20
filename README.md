76c3340df66b858c8ea67f8c0c6550a0faea2827
Josh Sherman <josh@gravityblvd.com>
2016-02-20 11:17:29 -0500

    :shipit:

---
+++ README.md
@@ -0,0 +1,28 @@
+# php-pixel
+
+> Yo Dawg, I herd you like pixels, so I put a pixel in your pixel so you can
+> pixel while you pixel.
+
+Why does this exist? Because I got sick of looking up how to generate a single
+transparent pixel in PHP.
+
+## Installation
+
+```
+composer require "joshtronic/php-pixel:dev-master"
+```
+
+## Usage
+
+### GIF
+
+```
+joshtronic\Pixel::gif();
+```
+
+### PNG
+
+```
+joshtronic\Pixel::png();
+```
+
---
+++ src/Pixel.php
@@ -0,0 +1,18 @@
+<?php
+namespace joshtronic;
+
+class Pixel
+{
+    public static function gif()
+    {
+        header('Content-Type: image/gif');
+        echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICBAEAOw==');
+    }
+
+    public static function png()
+    {
+        header('Content-Type: image/png');
+        echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
+    }
+}
+
